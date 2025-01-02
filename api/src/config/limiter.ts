72b82f6e9455a7604cc325a9a29f911aa576ac80
Todor <james@helloworld-team.com>
2025-01-02 10:51:32 +0100

    udpate api limiter

--- api/src/config/limiter.ts
+++ api/src/config/limiter.ts
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window`
+    windowMs: 1 * 60 * 1000, // 15 minutes
+    max: 50, // Limit each IP to 100 requests per `window`
     standardHeaders: true,
     legacyHeaders: false,
     message: {
